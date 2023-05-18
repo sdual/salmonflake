b3700c03940cb44ab464744bdde0e4c4d78d881b
sdual <shr.nrht.0623@gmail.com>
2023-05-18 19:10:42 +0900

    add config

--- snowflake.go
+++ snowflake.go
@@ -1,6 +1,9 @@
 package salmonflake
 
 import (
+	"fmt"
+	"time"
+
 	"github.com/sdual/salmonflake/dtype"
 )
 
@@ -10,6 +13,24 @@ type Salmonflake struct {
 	elapsedTime dtype.Time
 	sequence    dtype.Sequence
 	machineID   dtype.MachineID
+}
+
+type Config struct {
+	startTime time.Time
+	MachineID string
+}
+
+func New(conf Config) {
+	if err := validate(conf), err != nil {
+		panic("initialization error")
+	}
+}
+
+func validate(conf Config) error {
+	if conf.startTime.After(time.Now()) {
+		return fmt.Errorf("startTime must be before the current time. startTime: %w", conf.startTime)
+	}
+	return nil
 }
 
 // NextID generates a next unique ID.
